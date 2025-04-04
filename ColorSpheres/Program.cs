a615f558223e5763154c43554ebbf90214a18f40
ivyycore <mariasequeira.dev@gmail.com>
2025-04-04 11:02:13 +0100

    Made WriteLine

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -92,6 +92,17 @@ namespace ColorSpheres
             sphere2.Pop();
 
             sphere2.Throw();
+            
+            Console.WriteLine("Esfera 1:");
+            Console.WriteLine($"Cor: ({sphere1.GetColor().Red}, {sphere1.GetColor().Green}, {sphere1.GetColor().Blue})");
+            Console.WriteLine($"Raio: {sphere1.GetRadius()}");
+            Console.WriteLine($"Número de vezes que foi atirada: {sphere1.GetTimesThrown()}");
+            Console.WriteLine();
+
+            Console.WriteLine("Esfera 2:");
+            Console.WriteLine($"Cor: ({sphere2.GetColor().Red}, {sphere2.GetColor().Green}, {sphere2.GetColor().Blue})");
+            Console.WriteLine($"Raio: {sphere2.GetRadius()}");
+            Console.WriteLine($"Número de vezes que foi atirada: {sphere2.GetTimesThrown()}");
         }
     }
 }
