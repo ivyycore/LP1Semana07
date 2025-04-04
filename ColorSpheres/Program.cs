f469de72e0b36f087d99256fcde2368a0e12aec2
ivyycore <mariasequeira.dev@gmail.com>
2025-04-04 11:04:54 +0100

    Fixed more color bugs

--- ColorSpheres/Program.cs
+++ ColorSpheres/Program.cs
@@ -10,17 +10,17 @@ namespace ColorSpheres
             private byte green;
             private byte blue;
             
-            public byte red
+            public byte Red
             {
                 get { return red; }
                 set { red = value; }
             }
-            public byte green
+            public byte Green
             {
                 get { return green; }
                 set { green = value; }
             }
-            public byte blue 
+            public byte Blue 
             {
                 get { return blue; }
                 set { blue = value; }
