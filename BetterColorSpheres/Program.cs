c4b886c27409de3b3836dbd6fdf9f54f0fd68f0a
ivyycore <mariasequeira.dev@gmail.com>
2025-04-04 11:18:07 +0100

    Changed class Color

--- BetterColorSpheres/Program.cs
+++ BetterColorSpheres/Program.cs
@@ -4,32 +4,22 @@
     {
         public class Color
         {
-            private byte red;
-            private byte green;
-            private byte blue;
-            
-            public byte Red
-            {
-                get { return red; }
-                set { red = value; }
-            }
-            public byte Green
-            {
-                get { return green; }
-                set { green = value; }
-            }
-            public byte Blue 
-            {
-                get { return blue; }
-                set { blue = value; }
-            }
+            //Propriedades
+            public byte Red { get; }
+            public byte Green { get; }
+            public byte Blue { get; }
 
+
+            //Construtor
             public Color(byte red, byte green, byte blue)
             {
-                this.red = red;
-                this.green = green;
-                this.blue = blue;
+                Red = red;
+                Green = green;
+                Blue = blue;
             }
+
+            //readonly 
+             public byte Grey => (byte)((Red + Green + Blue) / 3);
         }
 
         private class Sphere
